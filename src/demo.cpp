b641cd055326936c32e4645d10825a74b47cbd74
ric <qqlilichong@163.com>
2017-11-21 07:51:27 +0800

    add 126 test

--- src/demo.cpp
+++ src/demo.cpp
@@ -8,11 +8,11 @@ int main()
 	using namespace secret ;
 	
 	service_meta service ;
-	service.smtp = "smtp.163.com" ;
-	service.pop3 = "pop3.163.com" ;
-	service.user = "user@163.com" ;
-	service.pawd = "password" ;
-	file_to_service( service, "g:/2017.jpg", "qqlilichong@163.com" ) ;
+	service.smtp = "smtp.126.com" ;
+	service.pop3 = "pop.126.com" ;
+	service.user = "testhackpro@126.com" ;
+	service.pawd = "123abc" ;
+	file_to_service( service, "g:/2017.jpg", "testhackpro@126.com" ) ;
 
 	return 0 ;
 }
