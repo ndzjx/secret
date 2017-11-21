4cdcbc99f4fe184dcb78230d541f30726de12fb3
ric <qqlilichong@163.com>
2017-11-21 09:39:49 +0800

    py2exe

--- src/demo.cpp
+++ src/demo.cpp
@@ -12,7 +12,17 @@ int main()
 	service.pop3 = "pop.126.com" ;
 	service.user = "testhackpro@126.com" ;
 	service.pawd = "123abc" ;
-	file_to_service( service, "g:/2017.jpg", "testhackpro@126.com" ) ;
+	
+	if ( false )
+	{
+		file_to_service( service, "g:/2017.jpg", "testhackpro@126.com" ) ;
+		return 0 ;
+	}
+
+	if ( true )
+	{
+		return 0 ;
+	}
 
 	return 0 ;
 }
