e00ffeaabce30952249e5f790a95affe2a62ba7c
ndzjx <ndzjx@github.com>
2017-11-23 14:50:01 +0800

    settingwidget

--- win/ecloud/CSettingWidget.cpp
+++ win/ecloud/CSettingWidget.cpp
@@ -1,7 +1,53 @@
 #include "CSettingWidget.h"
+#include <QListWidget>
+#include <QPushButton>
+#include <QLabel>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QInputDialog>
 
 CSettingWidget::CSettingWidget(QWidget *parent) : QWidget(parent)
 {
+    auto pLblEmail = new QLabel(QString::fromLocal8Bit("邮件列表:"), this);
+    auto pLstEmail = new QListWidget(this);
+    auto pVblLeft = new QVBoxLayout(this);
+    pVblLeft->addWidget(pLblEmail);
+    pVblLeft->addWidget(pLstEmail);
+    auto pLeftWidget = new QWidget(this);
+    pLeftWidget->setLayout(pVblLeft);
 
+    auto pBtnAdd = new QPushButton(QString::fromLocal8Bit("添加"));
+    auto pBtnDel = new QPushButton(QString::fromLocal8Bit("删除"));
+    auto pVblRight = new QVBoxLayout(this);
+    pVblRight->addStretch(2);
+    pVblRight->addWidget(pBtnAdd);
+    pVblRight->addStretch(1);
+    pVblRight->addWidget(pBtnDel);
+    pVblRight->addStretch(2);
+    auto pRightWidget = new QWidget(this);
+    pRightWidget->setLayout(pVblRight);
+
+    auto pHblCenter = new QHBoxLayout(this);
+    pHblCenter->addWidget(pLeftWidget);
+    pHblCenter->addWidget(pRightWidget);
+
+    connect(pBtnAdd, &QPushButton::clicked, [this, pLstEmail](){
+        QString sEmail = QInputDialog::getText(this, "Input", "Email:");
+        if (!sEmail.isEmpty())
+        {
+            pLstEmail->addItem(sEmail);
+        }
+    });
+    connect(pBtnDel, &QPushButton::clicked, [pLstEmail](){
+        if (pLstEmail->currentIndex().isValid())
+        {
+            auto pItem = pLstEmail->currentItem();
+            pLstEmail->removeItemWidget(pItem);
+            delete pItem;
+        }
+    });
+
+    setLayout(pHblCenter);
+    resize(800, 600);
 }
 
--- win/ecloud/ecloud.pro
+++ win/ecloud/ecloud.pro
@@ -4,15 +4,25 @@ QT       += core gui widgets
 greaterThan(QT_MAJOR_VERSION, 4): DEFINES += NOMINMAX
 greaterThan(QT_MAJOR_VERSION, 4): QT += core-private widgets-private
 
-TARGET = ecloud
 TEMPLATE = app
 
+DESTDIR = ../../run
+
+CONFIG(debug, debug|release){
+    win32:TARGET=$$join(TARGET,,,d)
+    win32:QMAKE_CXXFLAGS_DEBUG += -Fd$$DESTDIR/$$TARGET$$().pdb
+}
+
 HEADERS += \
-    CUploadWidget.h
+    CUploadWidget.h \
+    CBrowserWidget.h \
+    CSettingWidget.h
 
 SOURCES += \
     CUploadWidget.cpp \
-    main.cpp
+    main.cpp \
+    CBrowserWidget.cpp \
+    CSettingWidget.cpp
 
 RESOURCES  = \
     ecloud.qrc
