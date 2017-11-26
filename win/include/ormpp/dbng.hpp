4a875b3bb7dbba1cc8d1d2a205fbd307c2e8316a
ric <qqlilichong@163.com>
2017-11-27 07:40:52 +0800

    add ormpp

---
+++ win/include/ormpp/dbng.hpp
@@ -0,0 +1,137 @@
+//
+// Created by qiyu on 11/3/17.
+//
+
+#ifndef ORM_DBNG_HPP
+#define ORM_DBNG_HPP
+
+#include <string>
+#include <string_view>
+namespace ormpp{
+    template<typename DB>
+    class dbng{
+    public:
+        template <typename... Args>
+        bool connect(Args&&... args){
+            return  db_.template connect(std::forward<Args>(args)...);
+        }
+
+        bool disconnect(){
+            return  db_.disconnect();
+        }
+
+        template<typename T, typename... Args>
+        bool create_datatable(Args&&... args){
+            return db_.template create_datatable<T>(std::forward<Args>(args)...);
+        }
+
+        template<typename T, typename... Args>
+        int insert(const T& t,Args&&... args){
+            return db_.insert(t, std::forward<Args>(args)...);
+        }
+
+        template<typename T, typename... Args>
+        int insert(const std::vector<T>& t, Args&&... args){
+            return db_.insert(t, std::forward<Args>(args)...);
+        }
+
+        template<typename T, typename... Args>
+        int update(const T& t, Args&&... args) {
+            return db_.update(t, std::forward<Args>(args)...);
+        }
+
+        template<typename T, typename... Args>
+        int update(const std::vector<T>& t, Args&&... args){
+            return db_.update(t, std::forward<Args>(args)...);
+        }
+
+        template<typename T, typename... Args>
+        bool delete_records(Args&&... where_conditon){
+            return db_.template delete_records<T>(std::forward<Args>(where_conditon)...);
+        }
+
+        //restriction, all the args are string, the first is the where condition, rest are append conditions
+        template<typename T, typename... Args>
+        auto query(Args&&... args){
+            return db_.template query<T>(std::forward<Args>(args)...);
+        }
+
+        int execute(const std::string& sql){
+            return db_.execute(sql);
+        }
+
+        //transaction
+        bool begin(){
+            return db_.begin();
+        }
+
+        bool commit(){
+            return db_.commit();
+        }
+
+        bool rollback(){
+            return db_.rollback();
+        }
+
+    private:
+#define HAS_MEMBER(member)\
+template<typename T, typename... Args>\
+struct has_##member\
+{\
+private:\
+    template<typename U> static auto Check(int) -> decltype(std::declval<U>().member(std::declval<Args>()...), std::true_type()); \
+	template<typename U> static std::false_type Check(...);\
+public:\
+	enum{value = std::is_same<decltype(Check<T>(0)), std::true_type>::value};\
+};
+
+        HAS_MEMBER(before)
+        HAS_MEMBER(after)
+
+#define WARPER(func)\
+    template<typename... AP, typename... Args>\
+    auto warper##_##func(Args&&... args){\
+        using result_type = decltype(std::declval<decltype(this)>()->func(std::declval<Args>()...));\
+        bool r = true;\
+        std::tuple<AP...> tp{};\
+        for_each_l(tp, [&r, &args...](auto& item){\
+            if(!r)\
+                return;\
+            if constexpr (has_before<decltype(item)>::value)\
+            r = item.before(std::forward<Args>(args)...);\
+        }, std::make_index_sequence<sizeof...(AP)>{});\
+        if(!r)\
+            return result_type{};\
+        auto lambda = [this, &args...]{ return this->func(std::forward<Args>(args)...); };\
+        result_type result = std::invoke(lambda);\
+        for_each_r(tp, [&r, &result, &args...](auto& item){\
+            if(!r)\
+                return;\
+            if constexpr (has_after<decltype(item), result_type>::value)\
+            r = item.after(result, std::forward<Args>(args)...);\
+        }, std::make_index_sequence<sizeof...(AP)>{});\
+        return result;\
+    }
+
+        template <typename... Args, typename F, std::size_t... Idx>
+        constexpr void for_each_l(std::tuple<Args...>& t, F&& f, std::index_sequence<Idx...>)
+        {
+            (std::forward<F>(f)(std::get<Idx>(t)), ...);
+        }
+
+        template <typename... Args, typename F, std::size_t... Idx>
+        constexpr void for_each_r(std::tuple<Args...>& t, F&& f, std::index_sequence<Idx...>)
+        {
+            constexpr auto size = sizeof...(Idx);
+            (std::forward<F>(f)(std::get<size-Idx-1>(t)), ...);
+        }
+
+    public:
+        WARPER(connect);
+        WARPER(execute);
+    private:
+        DB db_;
+    };
+}
+
+#endif //ORM_DBNG_HPP
---
+++ win/include/ormpp/utility.hpp
@@ -0,0 +1,195 @@
+//
+// Created by qiyu on 10/29/17.
+//
+#ifndef ORM_UTILITY_HPP
+#define ORM_UTILITY_HPP
+#include "reflection.hpp"
+#include "entity.hpp"
+#include "type_mapping.hpp"
+
+namespace ormpp{
+    template<typename List>
+    struct result_size;
+
+    template<template<class...> class List, class... T>
+    struct result_size<List<T...>> {
+        constexpr static size_t value = (iguana::get_value<T>() + ...);
+    };
+
+    template<typename... Args>
+    inline void append(std::string& sql, Args&&... args) {
+        ((sql+=std::forward<Args>(args), sql+=" "),...);
+    }
+
+    template<typename... Args>
+    auto sort_tuple(const std::tuple<Args...>& tp){
+        if constexpr(sizeof...(Args)==2){
+            auto [a, b] = tp;
+            if constexpr(!std::is_same_v<decltype(a), ormpp_key>&&!std::is_same_v<decltype(a), ormpp_auto_increment_key>)
+            return std::make_tuple(b, a);
+            else
+            return tp;
+        }else{
+            return tp;
+        }
+    }
+
+    enum class DBType{
+        mysql,
+        sqlite,
+        postgresql
+    };
+
+    template <typename T>
+    inline constexpr auto get_type_names(DBType type){
+        constexpr auto SIZE = iguana::get_value<T>();
+        std::array<std::string_view, SIZE> arr = {};
+        iguana::for_each(T{}, [&](auto& item, auto i){
+            constexpr auto Idx = decltype(i)::value;
+            using U = std::remove_reference_t<decltype(iguana::get<Idx>(std::declval<T>()))>;
+            std::string_view s;
+            switch (type){
+                case DBType::mysql : s = ormpp_mysql::type_to_name(identity<U>{});
+                    break;
+                case DBType::sqlite : s = ormpp_sqlite::type_to_name(identity<U>{});
+                    break;
+                case DBType::postgresql : s = ormpp_postgresql::type_to_name(identity<U>{});
+                    break;
+            }
+
+            arr[Idx] = s;
+        });
+
+        return arr;
+    }
+
+    template <typename... Args, typename Func, std::size_t... Idx>
+    inline void for_each0(const std::tuple<Args...>& t, Func&& f, std::index_sequence<Idx...>) {
+        (f(std::get<Idx>(t)), ...);
+    }
+
+    template<typename T>
+    inline auto generate_insert_sql(bool replace){
+        std::string sql = replace?"replace into ":"insert into ";
+        constexpr auto SIZE = iguana::get_value<T>();
+        constexpr auto name = iguana::get_name<T>();
+        append(sql, name.data(), " values(");
+        for (auto i = 0; i < SIZE; ++i) {
+            sql+="?";
+            if(i<SIZE-1)
+                sql+=", ";
+            else
+                sql+=");";
+        }
+
+        return sql;
+    }
+
+    template<typename  T>
+    inline auto generate_auto_insert_sql(auto auto_key_map_, bool replace){
+        std::string sql = replace?"replace into ":"insert into ";
+        constexpr auto SIZE = iguana::get_value<T>();
+        constexpr auto name = iguana::get_name<T>();
+        append(sql, name.data());
+
+        std::string fields = "(";
+        std::string values = " values(";
+        auto it = auto_key_map_.find(name.data());
+        for (auto i = 0; i < SIZE; ++i) {
+            std::string field_name = iguana::get_name<T>(i).data();
+            if(it!=auto_key_map_.end()&&it->second==field_name)
+                continue;
+
+            values+="?";
+            fields+=field_name;
+            if(i<SIZE-1){
+                fields+=", ";
+                values+=", ";
+            }
+            else{
+                fields+=")";
+                values+=")";
+            }
+        }
+        append(sql, fields, values);
+        return sql;
+    }
+
+//    template <typename T>
+    bool is_empty(const std::string& t){
+        return t.empty();
+    }
+
+    template<typename T, typename... Args>
+    inline auto generate_delete_sql(Args&&... where_conditon){
+        std::string sql = "delete from ";
+        constexpr auto SIZE = iguana::get_value<T>();
+        constexpr auto name = iguana::get_name<T>();
+        append(sql, name.data());
+        if constexpr (sizeof...(Args)>0)
+        if(!is_empty(std::forward<Args>(where_conditon)...))
+            append(sql, " where ", std::forward<Args>(where_conditon)...);
+
+        return sql;
+    }
+
+    template<typename T>
+    inline bool has_key(const std::string& s){
+        auto arr = iguana::get_array<T>();
+        for (int i = 0; i < arr.size(); ++i) {
+            if(s.find(arr[i].data())!=std::string::npos)
+                return true;
+        }
+
+        return false;
+    }
+
+    template<typename T, typename... Args>
+    inline auto generate_query_sql(Args&&... args){
+        static_assert(sizeof...(Args)==0||sizeof...(Args)>0);
+        std::string sql = "select * from ";
+        constexpr auto SIZE = iguana::get_value<T>();
+        constexpr auto name = iguana::get_name<T>();
+        append(sql, name.data());
+
+        if constexpr (sizeof...(Args)>0){
+            int i = 0;
+            for_each0(std::tuple(std::forward<Args>(args)...), [&i, &sql](const auto& item){
+                if(i==0&&has_key<T>(item))
+                    append(sql, " where ", item);
+                else
+                    append(sql, " ", item);
+
+                i++;
+            }, std::make_index_sequence<sizeof...(Args)>{});
+        }
+
+        return sql;
+    }
+
+    template<typename T>
+    inline constexpr auto to_str(T&& t){
+        if constexpr(std::is_arithmetic_v<std::decay_t<T>>){
+            return std::to_string(std::forward<T>(t));
+        } else{
+            return std::string("'")+t+ std::string("'");
+        }
+    }
+
+    inline void get_str(std::string& sql, const std::string& s){
+        auto pos = sql.find_first_of('?');
+        sql.replace(pos, 1, " ");
+        sql.insert(pos, s);
+    }
+
+    template<typename... Args>
+    inline std::string get_sql(const std::string& o, Args&&... args){
+        auto sql = o;
+        std::string s = "";
+        (get_str(sql, to_str(args)), ...);
+
+        return sql;
+    }
+}
+
+#endif //ORM_UTILITY_HPP
